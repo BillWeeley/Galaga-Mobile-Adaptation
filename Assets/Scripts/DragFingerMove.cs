0343d3b1ba83fa74ebce0374789374ecb7ad9d3f
BillWeeley <61968397+BillWeeley@users.noreply.github.com>
2020-12-11 16:02:42 +0000

    Initial commit

---
+++ Assets/Scripts/DragFingerMove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragFingerMove : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    private Rigidbody2D rb;
+    private Vector2 direction;
+    private float moveSpeed = 10f;
+
+    // Use this for initialization
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        /*if (Input.touchCount > 0)
+        {
+            //Touch touch = Input.GetTouch(0);
+            if (Input.GetMouseButtonDown(0))
+            {
+
+            }
+            touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            touchPosition.z = 0;
+            direction = (touchPosition - transform.position);
+            rb.velocity = new Vector2(direction.x, direction.y) * moveSpeed;
+
+            if (touch.phase == TouchPhase.Ended)
+                rb.velocity = Vector2.zero;
+        }*/
+
+        if (Input.GetMouseButton(0))
+        {
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0;
+            direction = (mousePosition - transform.position);
+            rb.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
+
